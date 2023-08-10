97d1a894e5d7d8efd226dc2ccdc17d7e139877aa
YanaKholod <106437450+YanaKholod@users.noreply.github.com>
2023-08-10 13:45:13 +0300

    upd

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const app = require("./index");
 const mongoose = require("mongoose");
 
 const { DB_HOST, PORT = 8080 } = process.env;
-console.log(process.env);
 
 const mongooseOptions = {
   useNewUrlParser: true,
