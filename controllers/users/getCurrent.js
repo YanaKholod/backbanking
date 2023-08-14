6af534aba015aa2e3f4cd9297396a489f0e9e928
Yana <106437450+YanaKholod@users.noreply.github.com>
2023-08-14 14:22:38 +0300

    Update getCurrent.js

--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -2,11 +2,11 @@ const getCurrent = async (req, res) => {
   const { phone, fullName } = req.user;
 
   res.json({
-     user: {
-      phone: user.phone,
-      fullName: user.fullName,
-      role: user.role,
-    },
+    
+      phone,
+      fullName,
+    
+
   });
 };
 
