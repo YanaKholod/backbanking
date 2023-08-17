c2035117f5faaa1c55f56d29e4c209863a14758d
Yana <106437450+YanaKholod@users.noreply.github.com>
2023-08-17 16:37:12 +0300

    Update addCompany.js

--- controllers/companies/addCompany.js
+++ controllers/companies/addCompany.js
@@ -9,9 +9,21 @@ const addCompany = async (req, res) => {
 
   const { _id: owner } = req.user;
 
-  const result = await Company.create(...req.body, owner);
-  // User.update();
-  res.status(201).json(result);
+  // const result = await Company.create(...req.body, owner);
+  // // User.update();
+  // res.status(201).json(result);
+  const companyData = {
+    ...req.body,
+    owner, // Add the owner property to the company data
+  };
+
+  try {
+    const result = await Company.create(companyData);
+    res.status(201).json(result);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: "Server error" });
+  }
 };
 
 module.exports = addCompany;
