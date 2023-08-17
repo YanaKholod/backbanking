a22bd5bdb70d5fb497bcebc74a38dd498ab92d27
Yana <106437450+YanaKholod@users.noreply.github.com>
2023-08-17 13:53:13 +0300

    Update deleteCompany.js

--- controllers/companies/deleteCompany.js
+++ controllers/companies/deleteCompany.js
@@ -6,9 +6,9 @@ const deleteCompanyById = async (req, res) => {
   if (role !== "admin") {
     return res.status(403).json({ error: "Only admin can delete the company" });
   }
-  const { id } = req.params;
+  const { _id } = req.params;
 
-  const result = await Company.findByIdAndRemove(id);
+  const result = await Company.findByIdAndRemove(_id);
 
   if (!result) {
     throw new HttpError(404, "Not found 1");
