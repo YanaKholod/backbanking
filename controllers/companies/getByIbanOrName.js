53675f66d5c114efc110ca0ddc6d60455b5346c1
YanaKholod <106437450+YanaKholod@users.noreply.github.com>
2023-08-21 18:45:53 +0300

    upd

--- controllers/companies/getByIbanOrName.js
+++ controllers/companies/getByIbanOrName.js
@@ -3,23 +3,29 @@ const Company = require("../../models/company");
 
 const getByIbanOrName = async (req, res) => {
   const { targetValue } = req.params;
-  let company;
-  console.log("reqParams", req.params);
-  if (targetValue) {
-    company = await Company.findOne({ iban: targetValue });
+  let companies = [];
 
-    if (!company) {
-      company = await Company.findOne({ companyName: targetValue });
-    }
+  console.log("reqParams", req.params);
+  // if (targetValue) {
+  //   company = await Company.findOne({ iban: targetValue });
+
+  //   if (!company) {
+  //     company = await Company.findOne({ companyName: targetValue });
+  //   }
+  if (targetValue) {
+    // Search for companies matching either IBAN or company name
+    companies = await Company.find({
+      $or: [{ iban: targetValue }, { companyName: targetValue }],
+    });
   } else {
     throw new HttpError(400, "Invalid parameters");
   }
 
-  if (!company) {
-    throw new HttpError(404, "Company not found");
-  }
+  // if (!company) {
+  //   throw new HttpError(404, "Company not found");
+  // }
 
-  res.json(company);
+  res.json(companies);
 };
 
 module.exports = getByIbanOrName;
