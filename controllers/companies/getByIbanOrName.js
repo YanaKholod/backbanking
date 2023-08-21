8090b921bea0fdc2dcad6820ff45fd76f32fa9db
YanaKholod <106437450+YanaKholod@users.noreply.github.com>
2023-08-21 19:29:45 +0300

    upd2

--- controllers/companies/getByIbanOrName.js
+++ controllers/companies/getByIbanOrName.js
@@ -2,14 +2,14 @@ const { HttpError } = require("../../helpers");
 const Company = require("../../models/company");
 
 const getByIbanOrName = async (req, res) => {
-  const targetValue = req.params.targetValue;
+  const identifier = req.params.identifier;
   let companies = [];
-  console.log("VALUE", targetValue);
+  console.log("VALUE", identifier);
   console.log("REQUEST PARAMS", req.params);
 
-  if (targetValue) {
+  if (identifier) {
     companies = await Company.find({
-      $or: [{ iban: targetValue }, { companyName: targetValue }],
+      $or: [{ iban: identifier }, { companyName: identifier }],
     });
   } else {
     throw new HttpError(400, "Invalid parameters");
