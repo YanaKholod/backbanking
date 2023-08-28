904af3b05cc0d50dbe71d6f7288346833b05d992
Yana <106437450+YanaKholod@users.noreply.github.com>
2023-08-28 16:23:13 +0300

    Update auth-routing.js

--- routes/api/auth-routing.js
+++ routes/api/auth-routing.js
@@ -24,7 +24,7 @@ router.get("/current", authenticate, controlWrapper(authController.getCurrent));
 router.patch(
   "/change",
   authenticate,
-  validateBody(schemas.updateSchema),
+  // validateBody(schemas.updateSchema),
   controlWrapper(authController.updateUser)
 );
 
