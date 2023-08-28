fa62e87e4a29cc17754767e10cf3d8832f93a745
YanaKholod <106437450+YanaKholod@users.noreply.github.com>
2023-08-28 14:09:50 +0300

    udp updating users cards

--- controllers/users/updateUser.js
+++ controllers/users/updateUser.js
@@ -2,8 +2,34 @@ const bcrypt = require("bcrypt");
 const User = require("../../models/user");
 const { HttpError } = require("../../helpers");
 
+// const updateUser = async (req, res) => {
+//   const { phone, password, fullName } = req.body;
+
+//   const user = await User.findOne({ phone });
+
+//   if (!user) {
+//     throw new HttpError(404, "User not found");
+//   }
+
+//   if (password) {
+//     const hashPassword = await bcrypt.hash(password, 10);
+//     user.password = hashPassword;
+//   }
+
+//   if (fullName) {
+//     user.fullName = fullName;
+//   }
+
+//   await user.save();
+
+//   res.json({
+//     phone: user.phone,
+//     fullName: user.fullName,
+//     role: user.role,
+//   });
+// };
 const updateUser = async (req, res) => {
-  const { phone, password, fullName } = req.body;
+  const { phone, password, fullName, card } = req.body;
 
   const user = await User.findOne({ phone });
 
@@ -20,13 +46,20 @@ const updateUser = async (req, res) => {
     user.fullName = fullName;
   }
 
+  if (card) {
+    user.cards.push(card);
+  }
+
   await user.save();
 
   res.json({
     phone: user.phone,
     fullName: user.fullName,
     role: user.role,
+    cards: user.cards,
   });
 };
+
+module.exports = updateUser;
 
 module.exports = updateUser;
