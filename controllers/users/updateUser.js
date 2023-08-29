e24492b66dcb8c14e1b63b0f136a13baf2d44797
YanaKholod <106437450+YanaKholod@users.noreply.github.com>
2023-08-29 13:34:23 +0300

    upd

--- controllers/users/updateUser.js
+++ controllers/users/updateUser.js
@@ -1,50 +1,14 @@
-const bcrypt = require("bcrypt");
 const User = require("../../models/user");
 const { HttpError } = require("../../helpers");
 
-// const updateUser = async (req, res) => {
-//   const { phone, password, fullName } = req.body;
-
-//   const user = await User.findOne({ phone });
-
-//   if (!user) {
-//     throw new HttpError(404, "User not found");
-//   }
-
-//   if (password) {
-//     const hashPassword = await bcrypt.hash(password, 10);
-//     user.password = hashPassword;
-//   }
-
-//   if (fullName) {
-//     user.fullName = fullName;
-//   }
-
-//   await user.save();
-
-//   res.json({
-//     phone: user.phone,
-//     fullName: user.fullName,
-//     role: user.role,
-//   });
-// };
 const updateUser = async (req, res) => {
-  const { phone, card } = req.body;
+  const { id, card } = req.body;
 
-  const user = await User.findOne({ phone });
+  const user = await User.findOne({ id });
 
   if (!user) {
     throw new HttpError(404, "User not found");
   }
-
-  // if (password) {
-  //   const hashPassword = await bcrypt.hash(password, 10);
-  //   user.password = hashPassword;
-  // }
-
-  // if (fullName) {
-  //   user.fullName = fullName;
-  // }
 
   if (card) {
     user.cards.push(card);
@@ -61,5 +25,3 @@ const updateUser = async (req, res) => {
 };
 
 module.exports = updateUser;
-
-// module.exports = updateUser;
