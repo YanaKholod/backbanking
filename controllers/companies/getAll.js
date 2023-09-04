d13fa63163e123709d4a01227ae78589464844b2
Yana <106437450+YanaKholod@users.noreply.github.com>
2023-09-04 13:13:01 +0300

    Update getAll.js

--- controllers/companies/getAll.js
+++ controllers/companies/getAll.js
@@ -9,9 +9,18 @@ const getAllCompanies = async (req, res) => {
       .json({ error: "Only admin can retrieve all companies" });
   }
 
+  // try {
+  //   const companies = await Company.find({});
+  //   res.json(companies);
+  const { page = 1, perPage = 10 } = req.query;
+  const skip = (page - 1) * perPage;
+
   try {
-    const companies = await Company.find({});
+    const companies = await Company.find({})
+      .skip(skip)
+      .limit(parseInt(perPage));
     res.json(companies);
+
   } catch (error) {
     console.error("Error fetching companies:", error);
     res.status(500).json({ error: "Internal server error" });
