f157841d44b4e974faa249708d41e8212ea9b850
Yana <106437450+YanaKholod@users.noreply.github.com>
2023-09-07 14:06:09 +0300

    Update getAll.js

--- controllers/companies/getAll.js
+++ controllers/companies/getAll.js
@@ -9,44 +9,22 @@ const getAllCompanies = async (req, res) => {
       .json({ error: "Only admin can retrieve all companies" });
   }
 
-try {
+  try {
     const page = parseInt(req.query.page) || 1;
     const perPage = parseInt(req.query.perPage) || 10;
 
     const totalCompanies = await Company.countDocuments({});
     const totalPages = Math.ceil(totalCompanies / perPage);
 
-   const sortField = req.query.sortField || "companyName"; // Default sort by companyName
-    const sortOrder = req.query.sortOrder === "desc" ? -1 : 1;
-
-    // Filtering
-    const filter = {}; // You can add filtering conditions here based on your requirements
-    if (req.query.countryCode) {
-      filter.countryCode = req.query.countryCode;
-    };
-  if (req.query.companyName) {
-      filter.companyName = req.query.companyName;
-    }; 
-  if (req.query.balance) {
-      filter.balance = req.query.balance;
-    };
-    // Add more filter conditions as needed
-
-    const totalCompanies = await Company.countDocuments(filter);
-    const totalPages = Math.ceil(totalCompanies / perPage);
-
-    const companies = await Company.find(filter)
-      .sort({ [sortField]: sortOrder })
+    const companies = await Company.find({})
       .skip((page - 1) * perPage)
       .limit(perPage);
-    
 
     res.json({
       companies,
       totalPages,
       currentPage: page,
     });
-  
   } catch (error) {
     console.error("Error fetching companies:", error);
     res.status(500).json({ error: "Internal server error" });
