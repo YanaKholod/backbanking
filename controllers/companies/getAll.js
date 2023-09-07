320127b92ee7e13adbf4613287cea222bca6001b
Yana <106437450+YanaKholod@users.noreply.github.com>
2023-09-07 14:14:33 +0300

    Update getAll.js

--- controllers/companies/getAll.js
+++ controllers/companies/getAll.js
@@ -12,13 +12,24 @@ const getAllCompanies = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
     const perPage = parseInt(req.query.perPage) || 10;
+const sortBy = req.query.sortBy || "name"; // Default to sorting by name
+    const sortOrder = req.query.sortOrder || "asc"; // Default to ascending order
 
     const totalCompanies = await Company.countDocuments({});
     const totalPages = Math.ceil(totalCompanies / perPage);
 
-    const companies = await Company.find({})
-      .skip((page - 1) * perPage)
-      .limit(perPage);
+    let query = Company.find({}).skip((page - 1) * perPage).limit(perPage);
+
+    if (sortBy === "name") {
+      // Sort by name
+      query = query.sort({ name: sortOrder === "asc" ? 1 : -1 });
+    }
+
+    const companies = await query.exec();
+
+    // const companies = await Company.find({})
+    //   .skip((page - 1) * perPage)
+    //   .limit(perPage);
 
     res.json({
       companies,
