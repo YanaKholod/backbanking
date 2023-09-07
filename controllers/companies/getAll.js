4bdcf81fe8984ef3b7b9c601a20e143f60d91ced
Yana <106437450+YanaKholod@users.noreply.github.com>
2023-09-07 13:50:56 +0300

    Update getAll.js

--- controllers/companies/getAll.js
+++ controllers/companies/getAll.js
@@ -16,9 +16,30 @@ try {
     const totalCompanies = await Company.countDocuments({});
     const totalPages = Math.ceil(totalCompanies / perPage);
 
-    const companies = await Company.find({})
+   const sortField = req.query.sortField || "companyName"; // Default sort by companyName
+    const sortOrder = req.query.sortOrder === "desc" ? -1 : 1;
+
+    // Filtering
+    const filter = {}; // You can add filtering conditions here based on your requirements
+    if (req.query.countryCode) {
+      filter.countryCode = req.query.countryCode;
+    };
+  if (req.query.companyName) {
+      filter.companyName = req.query.companyName;
+    }; 
+  if (req.query.balance) {
+      filter.balance = req.query.balance;
+    };
+    // Add more filter conditions as needed
+
+    const totalCompanies = await Company.countDocuments(filter);
+    const totalPages = Math.ceil(totalCompanies / perPage);
+
+    const companies = await Company.find(filter)
+      .sort({ [sortField]: sortOrder })
       .skip((page - 1) * perPage)
       .limit(perPage);
+    
 
     res.json({
       companies,
