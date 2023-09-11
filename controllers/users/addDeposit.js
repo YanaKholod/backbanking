3781d08558dcd6631f77709c521f144489ab0e6d
Yana <106437450+YanaKholod@users.noreply.github.com>
2023-09-11 17:45:36 +0300

    Update addDeposit.js

--- controllers/users/addDeposit.js
+++ controllers/users/addDeposit.js
@@ -1,5 +1,6 @@
 const { HttpError } = require("../../helpers");
 const User = require("../../models/user");
+const mongoose = require("mongoose"); 
 
 const addDeposit = async (req, res) => {
   const { id } = req.params;
@@ -10,7 +11,18 @@ const addDeposit = async (req, res) => {
   if (!user) {
     throw new HttpError(404, "User not found");
   }
+  
+const card = user.cards.find((card) => card._id.equals(new mongoose.Types.ObjectId(fromCard.id)));
+ 
+  if (!card) {
+    throw new HttpError(400, "Card not found");
+  }
 
+  if (card.balance < sumOfDeposit) {
+    throw new HttpError(400, "Insufficient balance");
+  }
+  
+  card.balance -= amount;
   const deposit = {
     sumOfDeposit,
     fromCard: { cardType: fromCard.cardType, id: fromCard.id, },
