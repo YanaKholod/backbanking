94f8ad895fcc213f912e40aea6c217335a2558e6
Yana <106437450+YanaKholod@users.noreply.github.com>
2023-10-24 16:46:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ mongoose
        console.log("A client connected to the WebSocket");
       
       ws.on("message", (message) => {
+        consoloe.log(message, 'message);
         try {
           
           const senderRole = message.role;
